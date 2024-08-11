fdb7c9fa65f32f69fa5e7e8e6d468860bbe0ea8c
BebsYanski <bebongnchuy@gmail.com>
2024-08-11 05:38:08 +0100

    feat: Worked on a tabs projects; navigating through different tabs and displaying their values, kinda similar to the menu project/feature but with more functionalities.

--- src/06-tabs/starter/index.jsx
+++ src/06-tabs/starter/index.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import './index.css'
+import { FaAngleDoubleRight } from 'react-icons/fa'
 
 const url = 'https://www.course-api.com/react-tabs-project'
 
@@ -37,13 +38,51 @@ const App = () => {
       </section>
     )
   }
-  if (isError) {
+  /*  if (isError) {
     return (
       <section className='section container'>
         <h2>An Error Occurred</h2>
       </section>
     )
-  }
-  return <h2>Tabs Starter</h2>
+  } */
+
+  const { company, dates, duties, title } = jobs[value]
+  return (
+    <section>
+      <div className='title'>
+        <h2>experience</h2>
+        <div className='title-underline'></div>
+      </div>
+      <div className='jobs-center'>
+        {/* btn Container */}
+        <div className='btn-container'>
+          {jobs.map((item, index) => {
+            return (
+              <button
+                className={`job-btn ${index === value && 'active-btn'}`}
+                onClick={() => setValue(index)}
+                key={index}
+              >
+                {item.company}
+              </button>
+            )
+          })}
+        </div>
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4 className='job-company'>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className='job-desc'>
+                <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+    </section>
+  )
 }
 export default App
