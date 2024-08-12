6b6e3fc731f3bee6d41ca4a0fb804b6aa00f0171
BebsYanski <bebongnchuy@gmail.com>
2024-08-12 04:34:12 +0100

    Slider project completed. fully functional slider showing different reviews.

--- src/07-slider/starter/index.jsx
+++ src/07-slider/starter/index.jsx
@@ -1,11 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 import { FaQuoteRight } from 'react-icons/fa'
-import { longList as data } from './data'
+import { list as data } from './data'
+import './index.css'
 
 const App = () => {
   const [people, setPeople] = useState(data)
   const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    const lastIndex = people.length - 1
+    if (index < 0) {
+      setIndex(lastIndex)
+    }
+    if (index > lastIndex) {
+      setIndex(0)
+    }
+  }, [index, people])
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1)
+    }, 3000)
+    return () => clearInterval(slider)
+  }, [index])
 
   return (
     <section className='section'>
@@ -17,11 +35,23 @@ const App = () => {
       <div className='section-center'>
         {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person
+          let position = 'nextSlide'
+          if (personIndex === index) {
+            position = 'activeSlide'
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = 'lastSlide'
+          }
+
+          // let position = personIndex === index ? 'nextSlide' : 'activeSlide'
 
           return (
-            <article key={id}>
+            <article className={position} key={id}>
               <img className='person-img' src={image} alt={name} />
-              <h4>{name}</h4>
+              <h4 className='name'>{name}</h4>
               <p className='title'>{title}</p>
               <p className='text'>{quote}</p>
               <FaQuoteRight className='icon' />
@@ -29,10 +59,10 @@ const App = () => {
           )
         })}
         <button className='prev'>
-          <FiChevronLeft />
+          <FiChevronLeft onClick={() => setIndex(index - 1)} />
         </button>
         <button className='next'>
-          <FiChevronRight />
+          <FiChevronRight onClick={() => setIndex(index + 1)} />
         </button>
       </div>
     </section>
